ad9d638f3b11a0e051b7efc0407cdea32acd3810
Joan Raychouni <joan.raychouni@deutschebahn.com>
2020-05-26 17:04:57 +0200

    add gradient and dont show warning on init in manage

--- src/main/java/de/raychouni/ordernotifier/OrderManagerApplication.java
+++ src/main/java/de/raychouni/ordernotifier/OrderManagerApplication.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class OrderManagerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OrderManagerApplication.class, args);
     }
-
 }
