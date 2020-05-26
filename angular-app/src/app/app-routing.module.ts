5afbd8725c2ee584ac4c51981f40da90b4604d23
Joan Raychouni <joan.raychouni@deutschebahn.com>
2020-05-26 10:55:15 +0200

    provide redirect and angular rereouting

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {OrderListComponent} from "./order-list/order-list/order-list.component";
 import {OrderFormComponent} from "./order-form/order-form/order-form.component";
 
 const routes: Routes = [
-  { path: 'orders', component: OrderListComponent },
-  { path: 'manage', component: OrderFormComponent }
+  {path: 'orders', component: OrderListComponent},
+  {path: 'manage', component: OrderFormComponent},
+  {path: '', redirectTo: '/orders', pathMatch: 'full'},
+  {path: '**', component: OrderListComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
