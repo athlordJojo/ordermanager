78329d59014e0a0a710929b4211fa817a7a73c04
Joan Raychouni <joan.raychouni@deutschebahn.com>
2020-06-22 20:56:00 +0200

    orderapi test part1

--- angular-app/src/app/api/order-api.spec.ts
+++ angular-app/src/app/api/order-api.spec.ts
@@ -10,10 +10,11 @@ describe('Order-api', () => {
   let httpClientSpy
   let orderDto = new OrderDto();
   orderDto.title = "test order"
+  orderDto.uuid = "12345"
   const dummyResponse: OrderDto[] = [orderDto]
 
   beforeEach(() => {
-    const httpSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    const httpSpy = jasmine.createSpyObj('HttpClient', ['get', 'delete', 'put']);
 
     TestBed.configureTestingModule({
       // Provide both the service-to-test and its (spy) dependency
@@ -38,5 +39,22 @@ describe('Order-api', () => {
     service.findAll().subscribe(orders => {
       expect(orders).toEqual(dummyResponse)
     }, fail)
+    expect(httpClientSpy.get).toHaveBeenCalledWith("/companies/B28C343D-03C1-4FF1-90B9-5DDA8AFD3BFE/orders")
+  });
+
+  it('updates the given order expect success', () => {
+    httpClientSpy.put.and.returnValue(of("needs a parameter, otherwise oberserveable is not executed"));
+    service.update(orderDto).subscribe(orders => {
+      // nothing to check here
+    }, fail)
+    expect(httpClientSpy.put).toHaveBeenCalledWith("/companies/B28C343D-03C1-4FF1-90B9-5DDA8AFD3BFE/orders/12345", orderDto)
+  });
+
+  it('deletes the given order expect success', () => {
+    httpClientSpy.delete.and.returnValue(of("needs a parameter, otherwise oberserveable is not executed"));
+    service.delete(orderDto).subscribe(orders => {
+      // nothing to check here
+    }, fail)
+    expect(httpClientSpy.delete).toHaveBeenCalledWith("/companies/B28C343D-03C1-4FF1-90B9-5DDA8AFD3BFE/orders/12345")
   });
 });
